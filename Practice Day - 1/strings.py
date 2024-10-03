d94e7d954890fdb88f8416a6bbc1b5df5928abb8
S M Shahidullah Kaiser Talib <smshahidullahkaisertalib@gmail.com>
2024-10-03 22:37:11 +0600

    Add files via upload

---
+++ Practice Day - 1/strings.py
@@ -0,0 +1,16 @@
+
+# anem = "talib"
+
+# print(name+ "|"+ anem)
+# print(len(name))
+
+# print(name[3])
+
+# name[4] = "B" #we cant change index
+name = "talib"
+print(name[0:2])
+print(name.capitalize())
+print(name.replace("m","am"))
+print(len(name))
+print(name[-5:])
+
---
+++ Practice Day - 1/tuple.py
@@ -0,0 +1,26 @@
+# tuple = (1,2,3,)
+
+# t = tuple + ()
+# # print(t)
+# print(type(t))
+# print(tuple)
+
+# movies = []
+
+# movie = input("Please Enter 1st movie: ")
+# movies.append(movie)
+# movie = input("Please Enter 2nd movie: ")
+# movies.append(movie)
+# movie = input("Please Enter 3rd movie: ")
+# movies.append(movie)
+
+
+# print("favourite moives are ", movies)
+
+movies = []
+
+for i in range(1, 4):
+     movie = input("Please Enter movie: ")
+     movies.append(movie)
+
+print(movies)
